04505145fbfdfeaebf57480df8ecd9713dcf4d36
dudwns0213 <dudwns0213@gmail.com>
2024-05-02 06:48:36 +0900

    chore: svgr, prettier, eslint-config-prettier 설치
    svgr 사용 예시 App.jsx에 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import FaceBook from "./assets/icons/ic_facebook.svg?react";
+// svgr 사용 예시
+
 function App() {
   return (
     <>
       <h1>안녕 리액트!</h1>
+      <FaceBook />
     </>
   );
 }
