539820bb486685480990373ea46fb4cb7ae431de
dudwns0213 <dudwns0213@gmail.com>
2024-05-02 20:10:03 +0900

    fix: merge 충돌 해결(고재성)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import GlobalStyle from "./styles/GlobalStyle";
 import FaceBook from "./assets/icons/ic_facebook.svg?react"; // svgr 사용 예시
 import DropdownMenu from "./components/UI/DopdownMenu";
 import TextAreaItem from "./components/UI/TextAreaItem";
+import UserCard from "./pages/components/UserCard";
+import NoQuestion from "./components/UI/NoQuestion";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         label="상품 소개"
         placeholder="상품 소개를 입력해 주세요"
       />
+      <UserCard />
+      <NoQuestion />
     </>
   );
 }
