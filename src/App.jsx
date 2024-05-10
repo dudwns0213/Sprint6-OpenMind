0346e8aa8b1ec8182b1b89b415ba277edf34a52b
dudwns0213 <dudwns0213@gmail.com>
2024-05-10 20:05:06 +0900

    feat: 수정하기 기능만 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import FaceBook from "./assets/icons/ic_facebook.svg?react"; // svgr 사용 예시
 import QuestionsListPage from "./pages/QuestionsListPage.jsx";
@@ -7,6 +6,7 @@ import HomePage from "./pages/HomePage.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AnswerPage from "./pages/AnswerPage.jsx";
 import Feeds from "./pages/post/Feeds";
+import AnswersPatchPage from "./pages/AnswersPatchPage.jsx";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route index element={<AnswerPage />} />
+          <Route index element={<AnswersPatchPage />} />
           <Route path="/quetions" element={<QuestionsListPage />} />
           <Route path="/feeds" element={<Feeds />} />
         </Routes>
--- src/pages/QuestionsListPage.jsx
+++ src/pages/QuestionsListPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import getQuestions from "../api/OpenMindApi";
+import { getQuestions } from "../api/OpenMindApi";
 import QuestionsCard from "./QuestionsCard";
 
 function QuestionsListPage() {
