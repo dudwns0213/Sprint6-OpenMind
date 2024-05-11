6be60092694653e2dc67f7f7de7075be4698c2bf
Dongseok Lee <ehdtjr510@naver.com>
2024-05-11 15:06:36 +0900

    feature-이동석
    답변하기 버튼 미디어쿼리 + 코드 수정

--- src/components/UI/QuestionButtonTop.jsx
+++ src/components/UI/QuestionButtonTop.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ArrowIcon from "../../assets/icons/arrow_right.svg?react";
+import ArrowIcon from "../../assets/icons/arrow_right_brown.svg?react";
 
 const Button = styled.button`
   width: 161px;
@@ -10,11 +10,9 @@ const Button = styled.button`
   justify-content: center;
   background-color: #f5f1ee;
   color: #542f1a;
-  font-size: 16px;
-  font-weight: 400;
-  padding: 12px 24px;
   border: 1px solid #542f1a;
   border-radius: 8px;
+  gap: 8px;
   cursor: pointer;
   &:hover {
     border: 2px solid #542f1a;
@@ -27,17 +25,34 @@ const Button = styled.button`
     border: 1px solid #c7bbb5;
     cursor: not-allowed; /* 비활성화된 상태일 때 커서를 기본값으로 변경 */
   }
+  span {
+    font-family: Actor;
+    font-size: 16px;
+    font-weight: 400;
+    text-align: left;
+  }
+  @media (max-width: 676px) {
+    width: 127px;
+    height: 34px;
+    margin-top: 20px;
+    gap: 4px;
+    span {
+      font-family: Actor;
+      font-size: 14px;
+      font-weight: 400;
+      text-align: left;
+    }
+  }
 `;
-
 const StyledIcon = styled(ArrowIcon)`
   width: 18px;
   height: 18px;
 `;
 
-function QuestionButtonTop() {
+function QuestionButtonTop({ text }) {
   return (
     <Button>
-      질문하러 가기
+      <span>{text}</span>
       <StyledIcon />
     </Button>
   );
