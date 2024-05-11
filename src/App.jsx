62bf57a0050d6008708cc4fb223e754285b32f81
Dongseok Lee <ehdtjr510@naver.com>
2024-05-11 15:03:40 +0900

    feature-이동석
    app.jsx router 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import GlobalStyle from "./styles/GlobalStyle";
 import FaceBook from "./assets/icons/ic_facebook.svg?react"; // svgr 사용 예시
-import DropdownMenu from "./components/UI/DopdownMenu";
+import DropdownMenu from "./components/UI/DropdownMenu.jsx";
 import TextAreaItem from "./components/UI/TextAreaItem";
 import UserCard from "./pages/components/UserCard";
 import NoQuestion from "./components/UI/NoQuestion";
 import Feeds from "./pages/post/Feeds.jsx";
 import PostModal from "./pages/post/PostModal.jsx";
 import QuestionListUser from "./components/UI/QuestionListUser.jsx";
+import QuestionListPage from "./pages/QuestionListPage.jsx";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <h1>안녕 리액트!</h1>
-      <FaceBook />
-      <DropdownMenu />
-      <TextAreaItem
-        id="description"
-        label="상품 소개"
-        placeholder="상품 소개를 입력해 주세요"
-      />
-      <UserCard />
-      <NoQuestion />
-      <Feeds />
-      <PostModal />
-      <QuestionListUser />
-    </>
+    <BrowserRouter>
+    <GlobalStyle />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="list" element={<QuestionListPage />} />
+        <Route path="post/:itemId/answer" element={<QuestionListUser />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
