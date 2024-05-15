dc53ffb9763f4a1527f2597431f442fd59befcdb
Yeeun Cho <choange3@naver.com>
2024-05-15 14:39:20 +0900

    Docs: conflict난 파일 수정

--- src/components/UI/PostForm.jsx
+++ src/components/UI/PostForm.jsx
@@ -67,7 +67,6 @@ const PostForm = () => {
     try {
       const jsonObject = await getSubject();
 
-<<<<<<< HEAD
     const nameArray = jsonObject.results.map(item => item.name)
     userStorage.clear();
     if(nameArray.includes(userName)) {
@@ -107,52 +106,4 @@ return (
   </InputContainer>
 )
 }
-=======
-      const nameArray = jsonObject.results.map((item) => item.name);
-      userStorage.clear();
-      if (nameArray.includes(userName)) {
-        const nameIndex = nameArray.indexOf(userName);
-        const nameObject = jsonObject.results[nameIndex];
-        setUserId(nameObject.id);
-        userStorage.setItem(`userId`, nameObject.id);
-        navigate(`post/${nameObject.id}/answer`);
-      } else {
-        const response = await fetch(
-          "https://openmind-api.vercel.app/6-7/subjects/",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              name: userName,
-              team: "6-7",
-            }),
-          }
-        );
-        console.log(response);
-        const jsonResponse = await getSubject();
-        const nameObject = jsonResponse.results[0];
-        setUserId(nameObject.id);
-        userStorage.setItem(`userId`, nameObject.id);
-        navigate(`post/${nameObject.id}/answer`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <InputContainer>
-      <Input
-        placeholder="이름을 입력하세요"
-        type="text"
-        onChange={handleInput}
-      />
-      <QuestionButton onClick={handleSubmit}>질문 받기</QuestionButton>
-    </InputContainer>
-  );
-};
->>>>>>> b7ffc53522db17cf0ae712e6a02d1b5411ab575a
-
 export default PostForm;
